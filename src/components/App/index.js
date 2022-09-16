e9657b48e80c1e651f6b72114f2c2e45e4500a98
Dovydas Lozys <755086+DovLozys@users.noreply.github.com>
2022-09-16 13:14:27 +0100

    adds starter layout

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,24 @@
 function App() {
 	return (
-		<div>
-			Budgetinator
-		</div>
+		<>
+			<header>
+				<h1>Budgetinator</h1>
+				<form>
+					<label for="budget-data-file">Select a file:</label>
+					<input type="file" id="budget-data-file" name="budget-data-file" /><br /><br />
+					<input type="submit" value="Submit" />
+				</form>
+			</header>
+			<div className="row">
+				<div className="column side">
+					Month selector
+				</div>
+				<div className="column main">
+					<div>Month text data</div>
+					<div>Month charts</div>
+				</div>
+			</div>
+		</>
 	);
 }
 
