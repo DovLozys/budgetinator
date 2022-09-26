fa49eb87513a2e3ed55b357e124f1bf72c2420a7
Dovydas Lozys <755086+DovLozys@users.noreply.github.com>
2022-09-26 13:26:03 +0100

    refactor: extracts `MonthSelectorBox`

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
+
 import { FileSelector } from '../FileSelector';
+import { MonthSelectorBox } from '../MonthSelectorBox';
 
 function App() {
 	const [statementFile, setStatementFile] = useState([]);
@@ -11,25 +13,7 @@ function App() {
 			</header>
 			<div className="row">
 				<div className="column side">
-					<form action="/action_page.php">
-						Select a range: <br /><br />
-							<select name="month" id="month" size="12" multiple>
-								<option value="1">Jan</option>
-								<option value="2">Feb</option>
-								<option value="3">Mar</option>
-								<option value="4">Apr</option>
-								<option value="5">May</option>
-								<option value="6">Jun</option>
-								<option value="7">Jul</option>
-								<option value="8">Aug</option>
-								<option value="9">Sep</option>
-								<option value="10">Oct</option>
-								<option value="11">Nov</option>
-								<option value="12">Dec</option>
-							</select>
-						<br /><br />
-						<input type="submit" value="Submit" />
-					</form>
+					<MonthSelectorBox />
 				</div>
 				<div className="column main">
 					<div>Month text data</div>
