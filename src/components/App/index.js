f6ddac3187c36a7eaa1186590872c170e9193e00
Dovydas Lozys <755086+DovLozys@users.noreply.github.com>
2022-09-27 17:03:47 +0100

    adds a devx `PieChart` to the app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,18 @@
 import { useState } from 'react';
+import PieChart, {
+	Legend,
+	Series,
+	Tooltip,
+	Label,
+	Connector
+} from 'devextreme-react/pie-chart';
 
 import { FileSelector } from '../FileSelector';
 import { MonthSelectorBox } from '../MonthSelectorBox';
 
 import { getDebits, getCredits } from '../../models/fileParser';
+
+import 'devextreme/dist/css/dx.light.css';
 
 function App() {
 	const [statementFile, setStatementFile] = useState([]);
@@ -18,7 +27,22 @@ function App() {
 		});
 	}
 
+	// sums up the array returned by the model, rounds to two decimal places
+	function reducer(model) {
+		return Math.abs(Math.round(model(statementFile).reduce((partialSum, amount) => partialSum + amount, 0) * 100) / 100);
+	}
+
 	const isSelectionMultiple = monthRange[0] < monthRange[1];
+	const dataSource = [
+		{
+			type: 'credit',
+			val: reducer(getCredits),
+		},
+		{
+			type: 'debit',
+			val: reducer(getDebits),
+		},
+	];
 
 	return (
 		<>
@@ -36,14 +60,33 @@ function App() {
 							Summary for {getMonthName(monthRange[0])}
 							{isSelectionMultiple && " to " + getMonthName(monthRange[1])}
 						</h1>
-						<p>Total debits: {getDebits(statementFile).reduce((partialSum, amount) => partialSum + amount, 0)}</p>
-						<p>Total credits: {getCredits(statementFile).reduce((partialSum, amount) => partialSum + amount, 0)}</p>
+						<p>Total debits: {reducer(getDebits)}</p>
+						<p>Total credits: {reducer(getCredits)}</p>
 					</div>
 					<div>
 						<h2>
 							Charts for {getMonthName(monthRange[0])}
 							{isSelectionMultiple && " to " + getMonthName(monthRange[1])}
 						</h2>
+						<PieChart
+							id="pie"
+							type="pie"
+							title="CREDIT/DEBIT ratio"
+							palette="Soft Pastel"
+							dataSource={dataSource}
+						>
+							<Series argumentField="type">
+								<Label visible={true}>
+									<Connector visible={true} />
+								</Label>
+							</Series>
+							<Legend
+								margin={0}
+								verticalAlignment="top"
+								horizontalAlignment="right"
+							/>
+							<Tooltip enabled={true} />
+						</PieChart>
 					</div>
 				</div>
 			</div>
