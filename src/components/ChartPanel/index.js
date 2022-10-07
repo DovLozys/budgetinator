262200f9825008fd03d06f0e33703fc3a25863f6
Dovydas Lozys <755086+DovLozys@users.noreply.github.com>
2022-10-07 16:22:39 +0100

    adds skeleton `Popup` code to be used for displaying extra info about a selected month

--- src/components/ChartPanel/index.js
+++ src/components/ChartPanel/index.js
@@ -1,13 +1,29 @@
+import { useState } from 'react';
+
 import Chart, {
 	Series,
 	Label,
 	Connector,
 	Legend
 } from 'devextreme-react/chart';
+import { Popup, Position } from 'devextreme-react/popup';
 
 function ChartPanel(props) {
+	const [popupVisible, setPopupVisible] = useState(false);
+	const [currentMonth, setCurrentMonth] = useState();
+
 	if (props.statementFile.length === 0) {
 		return <><h2>Charts</h2><p>Select a statement file up top please.</p></>
+	}
+
+	function pointClickHandler(info) {
+		setPopupVisible(true);
+		setCurrentMonth(props.monthlyStatements[info.target.data.monthIndex].month);
+	}
+
+	function hidePopup() {
+		setPopupVisible(false);
+		setCurrentMonth();
 	}
 
 	return (
@@ -18,6 +34,7 @@ function ChartPanel(props) {
 			<Chart
 				title="Monthly debits"
 				dataSource={props.monthlyTotals}
+				onPointClick={pointClickHandler}
 				id="chart"
 			>
 				<Series type="bar">
@@ -29,6 +46,24 @@ function ChartPanel(props) {
 					visible={false}
 				/>
 			</Chart>
+			<Popup
+				visible={popupVisible}
+				onHiding={hidePopup}
+				dragEnabled={false}
+				hideOnOutsideClick={true}
+				title={`Stats for ${currentMonth}`}
+				width={300}
+				height={280}
+			>
+				<Position
+					at="center"
+					my="center"
+				/>
+				<p>
+					Month:
+					<span> {currentMonth}</span>
+				</p>
+			</Popup>
 		</>
 	)
 }
