3e3c34e26f4fa02f83667ad3ce09c859bd535f92
Dovydas Lozys <755086+DovLozys@users.noreply.github.com>
2022-10-07 15:09:27 +0100

    removes unused `PieChart`

--- src/components/ChartPanel/index.js
+++ src/components/ChartPanel/index.js
@@ -1,59 +1,19 @@
-import PieChart, {
-	Legend,
-	Series,
-	Tooltip,
-	Label,
-	Connector
-} from 'devextreme-react/pie-chart';
 import Chart, {
-	// Label,
-	// Legend,
-	// Series
+	Series,
+	Label,
+	Connector,
+	Legend
 } from 'devextreme-react/chart';
 
-import { getDebits, getCredits } from '../../models/fileParser';
-import { modelReducer } from '../../utils/utils';
-
-function ChartPanel({ statementFile, monthlyStatements, monthlyTotals }) {
-	const dataSource = [
-		{
-			type: 'credit',
-			val: modelReducer(getCredits, statementFile),
-		},
-		{
-			type: 'debit',
-			val: modelReducer(getDebits, statementFile),
-		},
-	];
-
+function ChartPanel(props) {
 	return (
 		<div>
 			<h2>
 				Charts
 			</h2>
-			<PieChart
-				id="pie"
-				type="pie"
-				title="CREDIT/DEBIT ratio"
-				palette="Soft Pastel"
-				dataSource={dataSource}
-			>
-				<Series argumentField="type">
-					<Label visible={true}>
-						<Connector visible={true} />
-					</Label>
-				</Series>
-				<Legend
-					margin={0}
-					verticalAlignment="top"
-					horizontalAlignment="right"
-				/>
-				<Tooltip enabled={true} />
-			</PieChart>
-			<br /><br />
 			<Chart
 				title="Monthly debits"
-				dataSource={monthlyTotals}
+				dataSource={props.monthlyTotals}
 				id="chart"
 			>
 				<Series type="bar">
