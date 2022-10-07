1a7b06d92e1660649abceae227cd32112ba8f491
Dovydas Lozys <755086+DovLozys@users.noreply.github.com>
2022-10-07 13:59:55 +0100

    implements grouping of transactions by month

--- src/components/FileSelector/index.js
+++ src/components/FileSelector/index.js
@@ -3,8 +3,7 @@ import { read, utils } from 'xlsx';
 function FileSelector(props) {
     function onChangeHandler(event) {
         event.preventDefault();
-        const transactionsByMonth = [[], [], [], [], [], [], [], [], [], [], [], []];
-
+        
         if (event.target.files) {
             const reader = new FileReader();
             reader.onload = (event) => {
@@ -13,17 +12,31 @@ function FileSelector(props) {
                 const sheetName = workbook.SheetNames[0];
                 const worksheet = workbook.Sheets[sheetName];
                 const arrayOfRows = utils.sheet_to_json(worksheet);
+                
+                // populate statementFile
                 props.setStatementFile(arrayOfRows);
-
+                
+                // populate monthlyStatements
+                const transactionsByMonth = [];
 				for (let i = 0; i < arrayOfRows.length; i++) {
-					const date = new Date(arrayOfRows[i]['Date']);
-					transactionsByMonth[date.getMonth()].push(arrayOfRows[i]);
+                    const date = new Date(arrayOfRows[i]['Date']);
+                    if (transactionsByMonth[date.getMonth()] === undefined) {
+                        transactionsByMonth[date.getMonth()] = {
+                            month: date.getMonth(),
+                            entries: [arrayOfRows[i]]
+                        }
+                    } else {
+                        transactionsByMonth[date.getMonth()].entries.push(arrayOfRows[i])
+                    }
 				}
+                props.setMonthlyStatements(transactionsByMonth);
+
+                // TODO: populate monthlyTotals using transactionsByMonth
+                const totals = [];
+
             }
             reader.readAsArrayBuffer(event.target.files[0]);
         }
-
-        props.setMonthlyStatements(transactionsByMonth);
     }
 
     return (
